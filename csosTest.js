aacf4c917de2c9beaa9e02dadb366f26f5257a2c
anarwal <anarwal@axway.com>
2015-12-03 10:55:28 -0700

    Test fix

--- csosTest.js
+++ csosTest.js
@@ -1,47 +1,17 @@
-var webdriverio = require('webdriverio'),
-    chai = require('chai');
-expect=chai.expect;
-
-
-var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync('test.json', 'utf8'));
+var chai = require('chai'),
+    expect = chai.expect,
+    obj = require('./test.json');
 
 describe('Test for CSOS UI', function() {
-    // set timeout to 10 seconds
-    this.timeout(10000);
-    var driver = {};
-    var popup;
-    var main;
-
-    before( function (done) {
-        driver = webdriverio.remote({ desiredCapabilities: {
-            browserName: 'phantomjs'
-            //          chromeOptions: {
-            //               "args": [
-            //                  '--load-extension='+'C:/Users/anarwal/AppData/Local/Google/Chrome/User Data/Default/Extensions/nlfbmbojpeacfghkpbjhddihlkkiljbi/1.0.3_0']}
-
-        } });
-        driver.init(done);
-    });
-
-
     it('should load correct Login page and title', function () {
-        return driver
+        return browser
             .windowHandleMaximize()
-            .url('https://google.com')
+            .url('http://google.com')
+            .pause(1000)
             .getTitle().then( function (title) {
                 expect(title).to.be.equal(obj.title);
             })
             .saveScreenshot('./screenshots/shot1.png')
             .end();
-    });
-
-
-    after(function(done) {
-        if(typeof browser === "undefined") {
-            driver.end(done);
-        } else {
-            done();
-        }
     });
 });
--- wdio-csos.js
+++ wdio-csos.js
@@ -5,7 +5,7 @@ exports.config = {
     ],
 
     capabilities: [{
-        browserName: 'phantomjs'
+        browserName: 'chrome'
     }],
     //
     // ===================
